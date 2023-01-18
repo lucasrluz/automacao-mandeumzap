2d73786ebb38edb7dc693b95eb10bbbb4e6b5ec4
lucasrluz <lucasr.luzbr@gmail.com>
2023-01-18 11:53:07 -0300

    identificadores de elementos

---
+++ automacao_mandeumzap/utils/elements_identifiers.py
@@ -0,0 +1,6 @@
+LOGIN_USERNAME = 'input[id="username"]'
+LOGIN_PASSWORD = 'input[id="password"]'
+LOGIN_BUTTON = '#app > div.sc-gLEhor.dVaUYJ > div > form > button'
+
+NOW = '#nowRoute'
+TICKET_REPORT = '#ticketReportsRoute'
