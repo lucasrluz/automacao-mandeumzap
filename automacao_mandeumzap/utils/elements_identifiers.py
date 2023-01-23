7510848f3ba1da986b81ffae069057a78d6845c0
lucasrluz <lucasr.luzbr@gmail.com>
2023-01-23 11:43:38 -0300

    novos identificadores de elementos adicionados

--- automacao_mandeumzap/utils/elements_identifiers.py
+++ automacao_mandeumzap/utils/elements_identifiers.py
@@ -4,3 +4,5 @@ LOGIN_BUTTON = '#app > div.sc-gLEhor.dVaUYJ > div > form > button'
 
 NOW = '#nowRoute'
 TICKET_REPORT = '#ticketReportsRoute'
+EXIBIR_FILTROS_BUTTON = '#app > div.sc-gRtYjc.eQCOlm.d-flex.flex-column.align-items-stretch > div:nth-child(2) > div > div > div.d-flex.align-items-center.justify-content-between.mb-4 > div:nth-child(2) > button'
+NOME_FILTER = '#app > div.sc-gRtYjc.eQCOlm.d-flex.flex-column.align-items-stretch > div:nth-child(2) > div > div > div:nth-child(3) > div.sc-gUQvok.goKBGZ.mb-4 > div > div:nth-child(1) > div > input'
