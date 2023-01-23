8bd8ad95c6032fb3069612fdd2e1ebbbd3bc1dec
lucasrluz <lucasr.luzbr@gmail.com>
2023-01-23 11:44:44 -0300

    remoção de async/await e adicionado scroll na página

--- automacao_mandeumzap/main.py
+++ automacao_mandeumzap/main.py
@@ -1,12 +1,14 @@
-import asyncio
-from playwright.async_api import async_playwright, Playwright, Page
+from playwright.sync_api import Playwright, sync_playwright, Page
 from automacao_mandeumzap.utils.elements_identifiers import (
     LOGIN_BUTTON,
     LOGIN_PASSWORD,
     LOGIN_USERNAME,
     NOW,
-    TICKET_REPORT
+    TICKET_REPORT,
+    EXIBIR_FILTROS_BUTTON,
+    NOME_FILTER
 )
+
 import time
 from dotenv import load_dotenv, dotenv_values
 
@@ -14,32 +16,38 @@ load_dotenv()
 
 env = dotenv_values()
 
-async def login(page: Page):
-    await page.locator(LOGIN_USERNAME).type(env['USERNAME'])
-    await page.locator(LOGIN_PASSWORD).type(env['PASSWORD'])
-    await page.locator(LOGIN_BUTTON).click()
+def login(page: Page):
+    page.locator(LOGIN_USERNAME).type(env['USERNAME'])
+    page.locator(LOGIN_PASSWORD).type(env['PASSWORD'])
+    page.locator(LOGIN_BUTTON).click()
 
-async def run(playwright: Playwright):
+def run(playwright: Playwright):
     chromium = playwright.chromium
-    browser = await chromium.launch(headless=False, channel='chrome')
-    page = await browser.new_page()
-    await page.goto('https://agilizzapromotora.mandeumzap.com.br/login')
+    browser = chromium.launch(headless=False, channel='chrome')
+    page = browser.new_page()
+    page.goto('https://agilizzapromotora.mandeumzap.com.br/login')
     
-    await login(page)
+    login(page)
 
-    while (True):
-        nowButton = page.locator(NOW)
-        
+    while (True):        
         time.sleep(1)
-        await page.evaluate('(NOW) => document.querySelector(NOW).click()', NOW)
+        page.evaluate('(NOW) => document.querySelector(NOW).click()', NOW)
+
+        page.locator(EXIBIR_FILTROS_BUTTON).click()
+        page.locator(NOME_FILTER).type('ATENDIMENTO')
+
+        time.sleep(1)
+        page.mouse.wheel(300, 300)
 
         time.sleep(10)
-        ticket_report_button = page.locator(TICKET_REPORT)
-        await page.evaluate('(TICKET_REPORT) => document.querySelector(TICKET_REPORT).click()', TICKET_REPORT)        
+        page.evaluate('(TICKET_REPORT) => document.querySelector(TICKET_REPORT).click()', TICKET_REPORT)        
 
-        time.sleep(10)
+        time.sleep(1)
+        page.mouse.wheel(300, 300)
 
-async def main():
-    async with async_playwright() as playwright:
-        await run(playwright)
-asyncio.run(main())
+        time.sleep(9)
+
+def main():
+    with sync_playwright() as Playwright:
+        run(Playwright)
+main()
